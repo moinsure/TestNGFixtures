7388149474ffafff3793bd437ec8ede90806144e
tim-hepster <tim.kalich@hepster.com>
2022-09-19 08:44:57 +0200

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,11 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 dependencies {
     testImplementation(kotlin("test"))
     compileOnly("org.testng:testng:7.6.0")
